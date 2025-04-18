b1f7271d9c6512bd92b0e7905d32fb7537a85ddf
caleb-stark <stark.caleba@gmail.com>
2025-04-17 17:02:36 -0700

    Initial Commit

---
+++ jeopardy-app/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  jeopardy-app
+//
+//  Created by Caleb Stark on 4/17/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
